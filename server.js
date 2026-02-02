2a58c4bcdc730f9f58c01b104be00ad5d6619616
santiago <agromero@itba.edu.ar>
2026-02-02 20:56:47 -0300

    ajustes para render

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const db = require('./db');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
