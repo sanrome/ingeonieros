d5b23c78482e60ef7ac00b226f5384debbd340a5
santiago <agromero@itba.edu.ar>
2026-02-04 19:14:33 -0300

    se arreglo la subida de archivos

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 const db = require('./db');
 const multer = require('multer');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,7 +12,11 @@ const PORT = process.env.PORT || 3000;
 // Configuración de Multer
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'public/uploads/');
+        const dir = 'public/uploads/';
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        cb(null, dir);
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
